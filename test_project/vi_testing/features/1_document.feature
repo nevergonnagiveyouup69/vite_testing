080c5eeafcf2fe7ef375766e5dc9de0c76c86455
Kaushal <kaushal.r@7edge.com>
2024-10-30 15:49:50 +0530

    MSM-1324 #time 5m feature: added new materials

--- test_project/vi_testing/features/1_document.feature
+++
@@ -1,7 +0,0 @@
-Feature: App functionality
-
-  Scenario: checks if conformation renders and button is clickable
-    Given John sees the conformation is loaded
-    Then John check the conformation
-    When John checks the close button
- 
--- test_project/vi_testing/step-definations/checkbox.test.jsx
+++ test_project/vi_testing/step-definations/checkbox.test.jsx
@@ -1,4 +1,7 @@
 import { loadFeature, describeFeature } from "@amiceli/vitest-cucumber"
+import { render, screen, waitFor } from '@testing-library/react'
+import { expect } from 'vitest';
+import App from '../src/App.jsx';
 
 const feature = await loadFeature('vi_testing/features/2_check_box.feature')
 
@@ -9,11 +12,30 @@ describeFeature(feature, ({ BeforeAllScenarios, AfterAllScenarios, BeforeEachSce
   AfterEachScenario(() => {})
 
   Scenario(`checkbox if checkbox renders and button is clickable`, ({ Given, Then }) => {
-      Given(`John sees the checkbox is loaded`, () => { 
-        
-      })
-      Then(`John check the checkbox`, () => { 
 
+      Given(`John sees the checkbox is loaded`, () => { 
+        render(<App />);
+        const textElement = screen.getByText(/save to test HMR/i);
+        expect(textElement).toBeInTheDocument();
+
+        // Select the button for use in the Then step
+        const button = screen.getByRole('button');   
+        expect(button).toBeInTheDocument();
+      })
+
+      Then(`John check the checkbox`,async () => { 
+        render(<App />);
+        const textElement = screen.getByText(/save to test HMR/i);
+        expect(textElement).toBeInTheDocument();
+
+        // Select the button for use in the Then step
+        const button = screen.getByRole('button');  
+        button.click();  // Simulate a click event
+
+        // Use waitFor to wait for the DOM update
+        await waitFor(() => {
+          expect(button.textContent).toBe('count is 1');  // Expect the updated value
+        });
       })
   })
 
